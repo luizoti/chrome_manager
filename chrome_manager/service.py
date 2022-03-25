d3de9e810e3fec3536c0a3c3a77035ab9d4343bb
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-03-25 20:16:03 -0300

    standalone service module

---
+++ chrome_manager/service.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+"""Create a webdriver service."""
+
+import os
+
+os.environ['WDM_LOG'] = '0'
+os.environ['WDM_LOG_LEVEL'] = '0'
+os.environ['WDM_PRINT_FIRST_LINE'] = 'False'
+
+from webdriver_manager.chrome import ChromeDriverManager
+from selenium.webdriver.chrome.service import Service
+
+def create_service():
+    """Return a webdriver service."""
+    return Service(ChromeDriverManager().install())
