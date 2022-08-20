b59334b75bac131378c13e19b32d1bb626999c64
Luiz Antonio Lazoti <luizlazoti@hotmail.com>
2022-08-20 16:35:44 -0300

    sintax update

--- setup.py
+++ setup.py
@@ -5,21 +5,20 @@ with open("README.md", encoding="utf-8") as readme_file:
     readme = readme_file.read()
 
 setuptools.setup(
-    name='chrome_manager',
+    name="chrome_manager",
     python_requires=">=3.6",
     long_description=readme,
     long_description_content_type="text/markdown",
     # packages=setuptools.find_packages(exclude=['tests']),
     include_package_data=True,
-    version='0.0.34',
-    description=(
-        'Library to auto install chrome for Selenium usage.'),
-    author='Luiz Antonio Lazoti',
-    author_email='luizlazoti@hotmail.com',
-    url='https://github.com/luizoti/chrome_manager.git',
-    keywords=['testing', 'selenium', 'driver', 'test automation'],
+    version="0.0.34",
+    description=("Module to auto install chrome for Selenium usage."),
+    author="Luiz Antonio Lazoti",
+    author_email="luizlazoti@hotmail.com",
+    url="https://github.com/luizoti/chrome_manager.git",
+    keywords=["testing", "selenium", "driver", "test automation"],
     install_requires=[
-        'requests',
-        'webdriver_manager @ git+https://github.com/luizoti/webdriver_manager',
-  ],
+        "requests",
+        "webdriver_manager @ git+https://github.com/luizoti/webdriver_manager",
+    ],
 )
